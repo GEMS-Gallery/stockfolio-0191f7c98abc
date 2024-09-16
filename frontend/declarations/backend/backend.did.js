baa308603a8dee446ca6a62bd1648af69d8bde7a
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-15 21:57:04 -0400

    0191f88b-dd18-766b-b825-944cba6a4b8a

--- frontend/declarations/backend/backend.did.js
+++ frontend/declarations/backend/backend.did.js
@@ -1,17 +1,4 @@
 export const idlFactory = ({ IDL }) => {
-  const HttpRequest = IDL.Record({
-    'url' : IDL.Text,
-    'method' : IDL.Text,
-    'body' : IDL.Vec(IDL.Nat8),
-    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
-  });
-  const HttpResponse = IDL.Record({
-    'body' : IDL.Vec(IDL.Nat8),
-    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
-    'status_code' : IDL.Nat16,
-  });
-  return IDL.Service({
-    'http_request' : IDL.Func([HttpRequest], [HttpResponse], ['query']),
-  });
+  return IDL.Service({ 'greet' : IDL.Func([], [IDL.Text], []) });
 };
 export const init = ({ IDL }) => { return []; };
--- frontend/declarations/backend/index.js
+++ frontend/declarations/backend/index.js
@@ -10,7 +10,7 @@ export { idlFactory } from "./backend.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  '6wqea-aqaaa-aaaab-qajba-cai';
+  'bgnui-oqaaa-aaaab-qbjrq-cai';
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
